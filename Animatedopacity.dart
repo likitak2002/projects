38cd990a0f2bad9a22cd41dcc24e72f01b59ddd8
Likita K <likitak2101@gmail.com>
2025-10-14 11:28:36 +0530

    Create Animatedopacity.dart

---
+++ Animatedopacity.dart
@@ -0,0 +1,67 @@
+// import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or press Run > Flutter Hot Reload in a Flutter IDE). Notice that the
+        // counter didn't reset back to zero; the application is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Foo animations'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+      var myOpacity = 1.0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedOpacity(
+              opacity: myOpacity,
+              duration: Duration(seconds: 2),
+              child: Container(color: Colors.black26, width: 100, height: 100),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  myOpacity = 0.0;
+                });
+              },
+              child: Text('Click here'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
